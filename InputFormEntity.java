1514651711506c4b9a1c60ac1f6156e5a47cf7f6
tom_pro_glb <15201729341_glb@qq.com>
2020-01-02 16:05:21 +0800

    the first java  pojo

---
+++ InputFormEntity.java
@@ -0,0 +1,279 @@
+package com.sinoservices.customized.erp2save.entity;
+
+public class InputFormEntity {
+	
+	//表主键
+	private String inptPk;
+	//货主代码
+	private String ownerCode;
+	//接口类型
+	private String inptType;
+	//发送方
+	private String sender;
+	//发送方地址
+	private String sendUrl;
+	//发送时间
+	private String sendTime;
+	//跟踪单号
+	private String trackNo;
+	//发送报文
+	private String sendData;
+	//是否发送赛飞
+	private String isSendSave;
+	//发送赛飞时间
+	private String sendSaveTime;
+	//创建时间
+	private String createTime;
+	//修改时间
+	private String modifyTime;
+	//创建人
+	private String createBy;
+	//修改人
+	private String modifyBy;
+	//备用字段1
+	private String def1;
+	//备用字段2
+	private String def2;
+	//备用字段3
+	private String def3;
+	//备用字段4
+	private String def4;
+	//备用字段5
+	private String def5;
+	//备用字段6
+	private String def6;
+	//备用字段7
+	private String def7;
+	//备用字段8
+	private String def8;
+	//备用字段9
+	private String def9;
+	//备用字段10
+	private String def10;
+	//备用字段11
+	private String def11;
+	//备用字段12
+	private String def12;
+	//备用字段13
+	private String def13;
+	//备用字段14
+	private String def14;
+	//备用字段15
+	private String def15;
+	//备用字段16
+	private String def16;
+	//备用字段17
+	private String def17;
+	//备用字段18
+	private String def18;
+	//备用字段19
+	private String def19;
+	//备用字段20
+	private String def20;
+	
+	
+	public String getInptPk() {
+		return inptPk;
+	}
+	public void setInptPk(String inptPk) {
+		this.inptPk = inptPk;
+	}
+	public String getOwnerCode() {
+		return ownerCode;
+	}
+	public void setOwnerCode(String ownerCode) {
+		this.ownerCode = ownerCode;
+	}
+	public String getInptType() {
+		return inptType;
+	}
+	public void setInptType(String inptType) {
+		this.inptType = inptType;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getSendUrl() {
+		return sendUrl;
+	}
+	public void setSendUrl(String sendUrl) {
+		this.sendUrl = sendUrl;
+	}
+	public String getSendTime() {
+		return sendTime;
+	}
+	public void setSendTime(String sendTime) {
+		this.sendTime = sendTime;
+	}
+	public String getTrackNo() {
+		return trackNo;
+	}
+	public void setTrackNo(String trackNo) {
+		this.trackNo = trackNo;
+	}
+	public String getSendData() {
+		return sendData;
+	}
+	public void setSendData(String sendData) {
+		this.sendData = sendData;
+	}
+	public String getIsSendSave() {
+		return isSendSave;
+	}
+	public void setIsSendSave(String isSendSave) {
+		this.isSendSave = isSendSave;
+	}
+	public String getSendSaveTime() {
+		return sendSaveTime;
+	}
+	public void setSendSaveTime(String sendSaveTime) {
+		this.sendSaveTime = sendSaveTime;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getModifyTime() {
+		return modifyTime;
+	}
+	public void setModifyTime(String modifyTime) {
+		this.modifyTime = modifyTime;
+	}
+	public String getCreateBy() {
+		return createBy;
+	}
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+	public String getModifyBy() {
+		return modifyBy;
+	}
+	public void setModifyBy(String modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+	public String getDef1() {
+		return def1;
+	}
+	public void setDef1(String def1) {
+		this.def1 = def1;
+	}
+	public String getDef2() {
+		return def2;
+	}
+	public void setDef2(String def2) {
+		this.def2 = def2;
+	}
+	public String getDef3() {
+		return def3;
+	}
+	public void setDef3(String def3) {
+		this.def3 = def3;
+	}
+	public String getDef4() {
+		return def4;
+	}
+	public void setDef4(String def4) {
+		this.def4 = def4;
+	}
+	public String getDef5() {
+		return def5;
+	}
+	public void setDef5(String def5) {
+		this.def5 = def5;
+	}
+	public String getDef6() {
+		return def6;
+	}
+	public void setDef6(String def6) {
+		this.def6 = def6;
+	}
+	public String getDef7() {
+		return def7;
+	}
+	public void setDef7(String def7) {
+		this.def7 = def7;
+	}
+	public String getDef8() {
+		return def8;
+	}
+	public void setDef8(String def8) {
+		this.def8 = def8;
+	}
+	public String getDef9() {
+		return def9;
+	}
+	public void setDef9(String def9) {
+		this.def9 = def9;
+	}
+	public String getDef10() {
+		return def10;
+	}
+	public void setDef10(String def10) {
+		this.def10 = def10;
+	}
+	public String getDef11() {
+		return def11;
+	}
+	public void setDef11(String def11) {
+		this.def11 = def11;
+	}
+	public String getDef12() {
+		return def12;
+	}
+	public void setDef12(String def12) {
+		this.def12 = def12;
+	}
+	public String getDef13() {
+		return def13;
+	}
+	public void setDef13(String def13) {
+		this.def13 = def13;
+	}
+	public String getDef14() {
+		return def14;
+	}
+	public void setDef14(String def14) {
+		this.def14 = def14;
+	}
+	public String getDef15() {
+		return def15;
+	}
+	public void setDef15(String def15) {
+		this.def15 = def15;
+	}
+	public String getDef16() {
+		return def16;
+	}
+	public void setDef16(String def16) {
+		this.def16 = def16;
+	}
+	public String getDef17() {
+		return def17;
+	}
+	public void setDef17(String def17) {
+		this.def17 = def17;
+	}
+	public String getDef18() {
+		return def18;
+	}
+	public void setDef18(String def18) {
+		this.def18 = def18;
+	}
+	public String getDef19() {
+		return def19;
+	}
+	public void setDef19(String def19) {
+		this.def19 = def19;
+	}
+	public String getDef20() {
+		return def20;
+	}
+	public void setDef20(String def20) {
+		this.def20 = def20;
+	}
+}
